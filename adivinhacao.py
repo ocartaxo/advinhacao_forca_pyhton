7b18914e57b5c65197a00808cbd83b5f17f24cbb
Otávio Augusto <otavioau.cartaxo@gmail.com>
2018-01-28 12:20:32 -0300

    rodadas adicionadas
    Signed-off-by: Otávio Augusto <otavioau.cartaxo@gmail.com>

--- adivinhacao.py
+++ adivinhacao.py
@@ -4,22 +4,32 @@ print("***************************************")
 
 numero_secreto = 42
 
-chute_str = input("Digite um número: ")
+tota_de_tentativas = 3
+rodada = 1
+ganhou = False
 
-print("Você digitou: ", chute_str)
+while (rodada <= tota_de_tentativas and not(ganhou) ):
 
-chute = int(chute_str)
+    print("Tentaiva", rodada, "de", tota_de_tentativas)
+    chute_str = input("Digite um número: ")
 
-acertou  = chute == numero_secreto
-eh_maior = chute > numero_secreto
-eh_menor  = chute < numero_secreto
+    print("Você digitou: ", chute_str)
 
-if(acertou):
-    print("Você acertou!")
-else:
-    if(eh_maior):
-        print("Você errou! O seu chute é maior que o número secreto.")
-    elif(eh_menor):
-        print("Você errou! O seu chute é menor que o número secreto.")
+    chute = int(chute_str)
+
+    acertou = chute == numero_secreto
+    eh_maior = chute > numero_secreto
+    eh_menor = chute < numero_secreto
+
+    if (acertou):
+        print("Você acertou!")
+        gahou = True
+    else:
+        if (eh_maior):
+            print("Você errou! O seu chute é maior que o número secreto.")
+        elif (eh_menor):
+            print("Você errou! O seu chute é menor que o número secreto.")
+
+    rodada += 1
 
 print("Fim de jogo!")
