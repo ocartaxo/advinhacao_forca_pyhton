9ca107a9b8f32d461814e43ef163cba1fab9b632
Otávio Augusto <otavioau.cartaxo@gmail.com>
2018-01-28 22:03:48 -0300

    função jogar adicionada
    Signed-off-by: Otávio Augusto <otavioau.cartaxo@gmail.com>

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,57 +1,59 @@
 import random
 
-print("*****************************************")
-print("*** Bem vindo ao jogo de Adivinhação! ***")
-print("*****************************************")
+def jogar():
 
-numero_secreto = round(random.randrange(1, 101))
-tota_de_tentativas = 0
-pontos = 1000
+    print("*****************************************")
+    print("*** Bem vindo ao jogo de Adivinhação! ***")
+    print("*****************************************")
 
-print("Qual o nível de dificuldade?")
-print("(1) Fácil  (2) Médio  (3) Difícil")
-dificuldade_valida = False
+    numero_secreto = round(random.randrange(1, 101))
+    tota_de_tentativas = 0
+    pontos = 1000
 
-while(not dificuldade_valida):
-    nivel = int(input("Defina a dificuldade: "))
+    print("Qual o nível de dificuldade?")
+    print("(1) Fácil  (2) Médio  (3) Difícil")
+    dificuldade_valida = False
 
-    if (nivel == 1):
-        tota_de_tentativas = 20
-        dificuldade_valida = True
-    elif (nivel == 2):
-        tota_de_tentativas = 10
-        dificuldade_valida = True
-    elif (nivel == 3):
-        tota_de_tentativas = 5
-        dificuldade_valida = True
-    else:
-        print("Por favor, insira um nível de dificuldade válido.")
+    while(not dificuldade_valida):
+        nivel = int(input("Defina a dificuldade: "))
 
-for rodada in range(1, tota_de_tentativas+1):
+        if (nivel == 1):
+            tota_de_tentativas = 20
+            dificuldade_valida = True
+        elif (nivel == 2):
+            tota_de_tentativas = 10
+            dificuldade_valida = True
+        elif (nivel == 3):
+            tota_de_tentativas = 5
+            dificuldade_valida = True
+        else:
+            print("Por favor, insira um nível de dificuldade válido.")
 
-    print("Tentaiva {} de {}".format(rodada, tota_de_tentativas))
-    chute_str = input("Chute um número inteiro entre 1 e 100: ")
-    print("Você digitou: ", chute_str)
-    chute = int(chute_str)
+    for rodada in range(1, tota_de_tentativas+1):
 
-    if(chute < 1 or chute > 100):
-        print("Você digitou um número inválido.")
-        continue
+        print("Tentaiva {} de {}".format(rodada, tota_de_tentativas))
+        chute_str = input("Chute um número inteiro entre 1 e 100: ")
+        print("Você digitou: ", chute_str)
+        chute = int(chute_str)
 
-    acertou = chute == numero_secreto
-    eh_maior = chute > numero_secreto
-    eh_menor = chute < numero_secreto
+        if(chute < 1 or chute > 100):
+            print("Você digitou um número inválido.")
+            continue
 
-    if (acertou):
-        print("Parabéns, você acertou! Sua pontuação foi {}".format(pontos))
-        acertou = True
-        break
-    else:
-        if (eh_maior):
-            print("O seu chute foi MAIOR que o número secreto.")
-        elif (eh_menor):
-            print("O seu chute foi MENOR que o número secreto.")
-        pontos_perdidos = abs(numero_secreto - chute)
-        pontos = pontos - pontos_perdidos
+        acertou = chute == numero_secreto
+        eh_maior = chute > numero_secreto
+        eh_menor = chute < numero_secreto
 
-print("Fim de jogo!")
+        if (acertou):
+            print("Parabéns, você acertou! Sua pontuação foi {}".format(pontos))
+            acertou = True
+            break
+        else:
+            if (eh_maior):
+                print("O seu chute foi MAIOR que o número secreto.")
+            elif (eh_menor):
+                print("O seu chute foi MENOR que o número secreto.")
+            pontos_perdidos = abs(numero_secreto - chute)
+            pontos = pontos - pontos_perdidos
+
+    print("Fim de jogo!")
