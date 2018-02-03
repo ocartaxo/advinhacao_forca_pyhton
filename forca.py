4499924afb813b51c54d69e6dd1a0cc342e98b7e
Otávio Augusto <otavioau.cartaxo@gmail.com>
2018-02-03 00:35:40 -0300

    procura todas as ocorrências da letra na palavra secreta
    Signed-off-by: Otávio Augusto <otavioau.cartaxo@gmail.com>

--- forca.py
+++ forca.py
@@ -10,8 +10,16 @@ def jogar():
     acertou  = False
 
     while(not enforcou and not acertou):
+
         chute = input("Chute uma letra: ")
 
+        index = 0
+        for letra in palavra_secreta:
+
+            if chute == letra:
+                print("Encontrei a letra {} na posição {}".format(chute, index))
+
+            index = index + 1
 
 
     print("Fim de jogo!")
