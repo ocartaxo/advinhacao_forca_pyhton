dc2284d1074215f857c258163af02db36bbc040e
Otávio Augusto <otavioau.cartaxo@gmail.com>
2018-02-03 00:31:47 -0300

    loop do jogo
    Signed-off-by: Otávio Augusto <otavioau.cartaxo@gmail.com>

--- forca.py
+++ forca.py
@@ -4,6 +4,16 @@ def jogar():
     print("*** Bem vindo ao jogo da Forca! ***")
     print("***********************************")
 
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou  = False
+
+    while(not enforcou and not acertou):
+        chute = input("Chute uma letra: ")
+
+
+
     print("Fim de jogo!")
 
 if(__name__ == "__main__"):
