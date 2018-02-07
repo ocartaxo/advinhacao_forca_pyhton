59b63582a58fdd9cc7762d3a31a30cb685998a1a
Otávio Augusto <otavioau.cartaxo@gmail.com>
2018-02-07 18:56:50 -0300

    mais funções
    Signed-off-by: Otávio Augusto <otavioau.cartaxo@gmail.com>

--- forca.py
+++ forca.py
@@ -1,52 +1,64 @@
 import random
 
-def jogar():
-    
-    imprime_mensagem_abertura()
+import random
 
+def jogar():
+
+    imprime_mensagem_abertura()
     palavra_secreta = carrega_palavra_secreta()
 
-    letras_acertadas = incializa_letras_acertadas(palavra_secreta)
-
-    enforcou = False
-    acertou  = False
-    erros = 0
-
+    letras_acertadas = inicializa_letras_acertadas(palavra_secreta)
     print(letras_acertadas)
 
-    while(not enforcou and not acertou):
+    enforcou = False
+    acertou = False
+    erros = 0
 
-        chute = input("Chute uma letra: ")
-        chute = chute.strip().upper()
+    while (not acertou and not enforcou):
 
-        if(chute in palavra_secreta):
-            index = 0
-            for letra in palavra_secreta:
-                if (chute.upper() == letra.upper()):
-                    letras_acertadas[index] = letra
-                index += 1
+        chute = pede_chute()
+
+        if (chute in palavra_secreta):
+            marca_chute_correto(chute, letras_acertadas, palavra_secreta)
         else:
             erros += 1
-            print("Ops, você errou. Agora voê tem {} tentativas.".format(6-erros))
 
         enforcou = erros == 6
         acertou = "_" not in letras_acertadas
         print(letras_acertadas)
 
-    if(acertou):
-        print("Parabéns, você ganhou!!")
+    if (acertou):
+        imprime_mensagem_vencedor()
     else:
-        print("Parabéns, você perdeu!!")
+        imprime_mensagem_perdedor()
 
-    print("Fim de jogo!")
+    print("Fim do jogo")
 
 
+def imprime_mensagem_vencedor():
+    print("Você ganhou!")
+
+def imprime_mensagem_perdedor():
+    print("Você perdeu!")
 
 def imprime_mensagem_abertura():
     # Mensagem de início
     print("***********************************")
     print("*** Bem vindo ao jogo da Forca! ***")
     print("***********************************")
+
+def marca_chute_correto(chute, palavra_secreta, letras_acertadas):
+    index = 0
+    for letra in palavra_secreta:
+        if (chute.upper() == letra.upper()):
+            letras_acertadas[index] = letra
+        index += 1
+
+
+def pede_chute():
+    chute = input("Chute uma letra: ")
+    chute = chute.strip().upper()
+    return chute
 
 def carrega_palavra_secreta():
     frutas = []
@@ -61,7 +73,7 @@ def carrega_palavra_secreta():
     return palavra_secreta
 
 
-def incializa_letras_acertadas(palavra):
+def inicializa_letras_acertadas(palavra):
     return ["_" for letra in palavra]  # Compreensão de lista (List Comprehension)
 
 
